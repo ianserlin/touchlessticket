798640f3e2dc030d061ccfdf4dd3157b06b94cc5
Geoffrey Chandler <geoffc@gmail.com>
2013-04-06 16:32:13 -0700

    formatting

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
-Meteor.publish('allTickets', function(){
-	return Tickets.find({});
+Meteor.publish('allTickets', function () {
+    return Tickets.find({});
 });
 
-Meteor.startup(function(){
-	if(Tickets.find({}).count() == 0){
-		seed();
-	}
+Meteor.startup(function () {
+    if (Tickets.find({}).count() == 0) {
+        seed();
+    }
 })
 
-function seed(){
-	for(var i = 0; i < 20; i++){
-		Tickets.insert({
-			index: (i+1)
-			, status: TicketStatus.NEW
-		});
-	}
+function seed() {
+    for (var i = 0; i < 15; i++) {
+        Tickets.insert({
+            index: (i + 1), status: TicketStatus.NEW
+        });
+    }
 }
