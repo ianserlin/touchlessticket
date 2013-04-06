6f07c053a1955a689ae0f27621cb2a2349b50203
Geoffrey Chandler <geoffc@gmail.com>
2013-04-06 15:58:26 -0700

    Created conversion function to get percentages on screen

--- client/leapclient.js
+++ client/leapclient.js
@@ -1,31 +1,47 @@
 // Setup Leap loop with frame callback function
 
-function convertHorizontalMillimetersToScreenPercent(mm){
-	return mm;
+function convertHorizontalMillimetersToScreenPercent(mm) {
+    return mm;
 }
 
-function convertVerticalMillimetersToScreenPercent(mm){
-	return mm;
+function convertVerticalMillimetersToScreenPercent(mm) {
+    return mm;
 }
+
+var rangeX = 400;
+var rangeY = 150;
+var offsetX = 200;
+var offsetY = 150;
+
+function convertCoordinates(mmX, mmY) {
+
+    var left = (mmX * -1) + offsetX;
+    var percentLeft = left / rangeX;
+
+    var top = (mmY - offsetY) * -1;
+    var bottom = offsetY + rangeY;
+    var percentTop = top / bottom;
+
+    return {x: percentLeft, y: percentTop};
+
+}
+
 
 var controllerOptions = {
-	enableGestures: true
+    enableGestures: true
 };
 
-Leap.loop(controllerOptions, function(frame) {
-	// Body of callback function
-	var fingers = frame.fingers;
-	if(fingers && fingers.length > 0){
-		var finger = fingers[0];
-		// console.log(finger.tipPosition[0], finger.tipPosition[1]);
-		if(finger){
-			var position = {
-				x: convertHorizontalMillimetersToScreenPercent(finger.tipPosition[0])
-				, y: convertVerticalMillimetersToScreenPercent(finger.tipPosition[1])
-			};
-			// console.log(position);
-			Session.set('pointerPosition', position);
-			console.log(finger.tipPosition);
-		}
-	}
+Leap.loop(controllerOptions, function (frame) {
+    // Body of callback function
+    var fingers = frame.fingers;
+    if (fingers && fingers.length > 0) {
+        var finger = fingers[0];
+        // console.log(finger.tipPosition[0], finger.tipPosition[1]);
+        if (finger) {
+            var position = convertCoordinates(finger.tipPosition[0], finger.tipPosition[1]);
+            // console.log(position);
+            Session.set('pointerPosition', position);
+            // console.log(finger.tipPosition);
+        }
+    }
 })
