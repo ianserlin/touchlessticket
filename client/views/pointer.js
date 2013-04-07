6229662c2a129f74a4022f4acf43ced2fc0c2fda
Geoffrey Chandler <geoffc@gmail.com>
2013-04-07 14:48:33 -0700

    new gesture for headsup display

--- client/views/pointer.js
+++ client/views/pointer.js
@@ -2,15 +2,12 @@ Template.pointer.position = function () {
     return Session.get('pointerPosition');
 };
 
+Template.motionMetrics.hitTestPosition = function () {
+    return Session.get('hitTestPosition');
+};
+
 Template.motionMetrics.fingers = function () {
     return Session.get('fingers');
 };
 
-Template.motionMetrics.lastSelected = function () {
-    return Session.get('lastSelected');
-};
-
-Template.motionMetrics.selectedElement = function () {
-    return Session.get('selectedElement');
-};
 
